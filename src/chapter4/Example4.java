112662b10c6ee6f2ac9b1f7e5e9239ef6a9d78b9
Ryan Desmond <radesmond@gmail.com>
2017-02-23 15:57:18 -0800

    first commit

---
+++ src/chapter4/Example4.java
@@ -0,0 +1,12 @@
+package chapter4;
+
+/*
+    void myMeth() {
+      int i;
+
+      for(i=0; i<10; i++) {
+        if(i == 5) return; // stop at 5
+        System.out.println();
+      }
+    }
+*/
