a9b90c106e674c39f8b2b653bfb5895ba2548999
Daniel Kapitanov <dskapitanov@gmail.com>
2017-07-03 15:01:12 +0700

    added selftest text file

--- src/main/java/fundamentals/Example1.java
+++ src/main/java/fundamentals/Example1.java
@@ -2,8 +2,22 @@ package main.java.fundamentals;
 
 class Example1 {
     // A Java program begins with a call to main().
-    public static void main(String[] args){
+    public static void main (String[] args){
 
-        System.out.println("Hello World! LMAO");
+        double inches, meters;
+        int counter;
+
+        counter = 0;
+        for(inches = 1; inches <= 120; inches++) {
+            meters = inches/39.37; // convert to meters
+            System.out.println(inches + " inches is " +
+                    meters + " meters");
+            counter++;
+            // every 10th line, print a blank line
+            if(counter == 12) {
+                System.out.println();
+                counter = 0; // reset the line counter
+            }
+        }
     }
 }
