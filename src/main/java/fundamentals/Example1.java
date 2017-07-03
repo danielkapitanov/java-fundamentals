1c72ee964ee005d4cc5bf9bc5ec5b59acd36f8e3
Daniel Kapitanov <dskapitanov@gmail.com>
2017-07-03 10:16:21 +0700

    edited Example 1 to say Hello World!

--- src/main/java/fundamentals/Example1.java
+++ src/main/java/fundamentals/Example1.java
@@ -4,6 +4,6 @@ class Example1 {
     // A Java program begins with a call to main().
     public static void main(String[] args){
 
-        System.out.println("Java drives the Web.");
+        System.out.println("Hello World!");
     }
 }
